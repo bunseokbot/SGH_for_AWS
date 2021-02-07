ef18a3c4f298e7bbb875798158fb4538e7309c2b
한재원 <hanjo8813@gmail.com>
2021-02-08 01:34:58 +0900

    test2

--- Graduate_Web/app/views.py
+++ Graduate_Web/app/views.py
@@ -224,11 +224,11 @@ def r_result(request):
     
     # 2. 중필(교필) 필수과목. { 학수번호 : 그룹번호 } 딕셔너리로 매핑
     # ind로 필수과목 추출후 딕셔너리 만들기
-    dic_ce = make_dic([s_num for s_num in s_row.ce_list.split(',')])
+    dic_ce = make_dic([s_num for s_num in s_row.ce_list.split('/')])
     # 3. 중선(교선1) 필수과목
-    dic_cs = make_dic([s_num for s_num in s_row.cs_list.split(',')])
+    dic_cs = make_dic([s_num for s_num in s_row.cs_list.split('/')])
     # 4. 기교 필수과목 
-    dic_b = make_dic([s_num for s_num in s_row.b_list.split(',')]) 
+    dic_b = make_dic([s_num for s_num in s_row.b_list.split('/')]) 
 
     #------------------------------------------------------------------------------
     # user_grade 테이블에서 사용자의 성적표를 DF로 변환하기
